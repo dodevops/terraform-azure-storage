ca45b56aca87cd50d0fc411f88523c31f69ae7c2
timdeluxe <5765175+timdeluxe@users.noreply.github.com>
2021-10-11 16:39:39 +0200

    feat(DO-779): First version of module

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_storage_account" "account" {
+  account_replication_type = var.replication_type
+  account_tier             = var.tier
+  location                 = var.location
+  name                     = "${lower(var.project)}${lower(var.stage)}storacc${var.suffix}"
+  resource_group_name      = var.resource_group
+  allow_blob_public_access = var.allow_blob_public_access
+  static_website {
+    index_document     = var.static_website_index_document
+    error_404_document = var.static_website_error_404_document
+  }
+}
+
+resource "azurerm_storage_container" "container" {
+  name                 = "${lower(var.project)}${lower(var.stage)}storcont${var.suffix}"
+  storage_account_name = azurerm_storage_account.account.name
+}
+
+resource "azurerm_storage_blob" "blob" {
+  name                   = "${lower(var.project)}${lower(var.stage)}storblob${var.suffix}"
+  storage_account_name   = azurerm_storage_account.account.name
+  storage_container_name = azurerm_storage_container.container.name
+  type                   = "Block"
+}
+
+resource "azurerm_storage_container" "additional_container" {
+  count                 = length(var.additional_containers)
+  name                  = "${lower(var.project)}${lower(var.stage)}storcont${var.additional_containers[count.index]["suffix"]}"
+  storage_account_name  = azurerm_storage_account.account.name
+  container_access_type = var.additional_containers[count.index]["access_type"]
+}
---
+++ output.tf
@@ -0,0 +1,30 @@
+output "access_key" {
+  value = azurerm_storage_account.account.primary_access_key
+}
+
+output "connection_string" {
+  value = azurerm_storage_account.account.primary_connection_string
+}
+
+output "blob_connection_string" {
+  value = azurerm_storage_account.account.primary_blob_connection_string
+}
+
+output "containers" {
+  value = join(" ", concat(
+    [
+      "${azurerm_storage_container.container.name}:${azurerm_storage_account.account.name}:${azurerm_storage_container.container.name}"
+      ], [
+      for cont in azurerm_storage_container.additional_container : "${cont.name}:${azurerm_storage_account.account.name}:${cont.name}"
+    ]
+  ))
+}
+
+
+output "static_host" {
+  value = azurerm_storage_account.account.primary_web_host
+}
+
+output "blob_host" {
+  value = azurerm_storage_account.account.primary_blob_host
+}
